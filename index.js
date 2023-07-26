80d9899d24463071928212153edc7fe3c84521c8
Riddhikedia <riddhikedia2021@gmail.com>
2023-07-26 14:29:00 +0530

    added fronted url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://random-gc-frontend.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
@@ -31,6 +31,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log("SERVER RUNNING");
 });
